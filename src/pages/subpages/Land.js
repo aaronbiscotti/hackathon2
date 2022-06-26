6f4a61c18eea49246a8cb378bca58a526e5a4d4b
aaronbiscotti <thesoupmanguy@gmail.com>
2022-06-25 21:53:10 -0400

    test

--- src/pages/subpages/Land.js
+++ src/pages/subpages/Land.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ae01d92c262a150b2e213e9fb3d22193ee829307591cd9758939368fe4b3511a
-size 5171
+oid sha256:d386fb44d0646074bac6909e8f688e1eeb8cdd5bc817b13915c21b720916aa3a
+size 6645
--- src/pages/subpages/Ocean.js
+++ src/pages/subpages/Ocean.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:71a130e661bcf023f89b83221db9d06e5484756ca262a559ff44c5b6ba77a282
-size 5569
+oid sha256:f461772985f0e9a05e486a2db864e909b40c3b88bd6aba4aef51685d2b159ec0
+size 7031
