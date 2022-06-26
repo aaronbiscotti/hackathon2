9874338c1c0bd00cda9ac10685d8141b2cdf8f50
aaronbiscotti <thesoupmanguy@gmail.com>
2022-06-25 22:30:58 -0400

    link

--- src/pages/subpages/Land.js
+++ src/pages/subpages/Land.js
@@ -66,8 +66,7 @@ const Land = () => {
     map.on("load", () => {
       map.addSource("wildfires", {
         type: "geojson",
-        // Use a URL for the value for the `data` property.
-        data: "https://wildfiredata2.s3.us-east-1.amazonaws.com/wildfire_data.geojson?response-content-disposition=inline&X-Amz-Security-Token=IQoJb3JpZ2luX2VjEJr%2F%2F%2F%2F%2F%2F%2F%2F%2F%2FwEaCXVzLWVhc3QtMSJGMEQCIAEplVBSfQAYu3hf%2FinRK6s6UQjI1EP1JXJjAwwYxA0MAiBxFnNSurOt%2Bedc0gB1VNZxe2vb6%2FECJqah%2FoukIu53GyrtAgiy%2F%2F%2F%2F%2F%2F%2F%2F%2F%2F8BEAEaDDEwMjU3MTQzODg2MyIMpekTO%2FwRnCERJLk6KsEC9y%2B9DXhMFpU1cQyunUpY80YaaBOOVsrwWqS5Jm3qUgLDZ%2B9zfKD4%2Bi3J7ui4Yj2ZbS%2F1cE3FXyzk2cIlsbOExvJKDzOazZKY5K0%2BnlFVUcnAK9dZ6jDExZEToGzNZeN7Or03ue2XqAeu02Uw3IGVAyj0sxnSjNuKpaOKfzDv9mgJGCtY8QqJKJVy9Nny6ZBXp0Mp0mySx3Xux8VnLJDnWFEjogua1QEgFFTorXbCNtWebYhZcZBN2TaHAsJMWYS7t8n1L8hEOiR%2BV%2FODtFFPjJMqyFm9q1zFamK6cc%2B7hjirWp0EU5yi65e4xN1Au9PXP6aHy3IKq9FOiKjb6jKYWiYGzOgaYhcoB2pvmLdUroK5I8rme2Fqgppszx88PM0ydpvExhLmJWyQ%2FOUK4TowZeebKZHDA4mnMbJtz%2BBpQdc1MPjj3pUGOrQCrfDSQC2wvcAHi3k7tBsdIj6H5%2F0dPXvvnZ3SOnTPT4jk%2FxWdKLRabsJX5E9WRQ0d9A1Squ2W%2BCxSMbnuo6CBM5x19zJcBTeefBnGN1%2FQ2suVB39%2BNbWNlvkGhTzrmOTLE7pC2sFrH1kHv9tYHQqEKfX3Cy5fYakMNAKsoE4PBmoBjonOzlfQa3MNmU96Il6n%2FqLBgSIeGCpuEJSKrZC%2BqpZ4IhgEx9J2EM6Yl0BG%2FXGEJyMZrzFTMMybktxEZHfzdoMgTLPWyW7lnaOx8RaeN6FZvOYjztM7iHDsiMC8TCMp4YThXNukgN%2FJ3ChlGylfVaHdHu4%2BDgWgvZdBk9%2Bo%2FdgoWTvZzkoy99RhhSSMUCIdtdTOhY2BqIocbEaYh4qur5l2Nr63Onry4%2F%2FuBt79gLAxeHY%3D&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Date=20220626T014923Z&X-Amz-SignedHeaders=host&X-Amz-Expires=43200&X-Amz-Credential=ASIARPYN36MH4XULSANX%2F20220626%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Signature=a7e98cfb72aff85d2d46bdc5c847823906d71e8140bd7f46237a3891cdb0259b",
+        data: "https://wildfiredata2.s3.amazonaws.com/wildfire_data.geojson",
       });
 
       map.addLayer({
@@ -108,7 +107,7 @@ const Land = () => {
             CO2 are released into the atmosphere and compounds our warming
             crisis at an alarming rate.
           </p>
-          <div className="w-full flex justify-center items-center mt-10">
+          <div className="w-full flex justify-center items-center mt-10 font-Messina">
             <ComposedChart width={830} height={250} data={data}>
               <XAxis dataKey="name" />
               <YAxis />
--- src/pages/subpages/Ocean.js
+++ src/pages/subpages/Ocean.js
@@ -22,8 +22,7 @@ const Ocean = () => {
     map.on("load", () => {
       map.addSource("coral-reefs", {
         type: "geojson",
-        // Use a URL for the value for the `data` property.
-        data: "https://wildfiredata2.s3.us-east-1.amazonaws.com/coral_bleaching.geoJson?response-content-disposition=inline&X-Amz-Security-Token=IQoJb3JpZ2luX2VjEJr%2F%2F%2F%2F%2F%2F%2F%2F%2F%2FwEaCXVzLWVhc3QtMSJGMEQCIAEplVBSfQAYu3hf%2FinRK6s6UQjI1EP1JXJjAwwYxA0MAiBxFnNSurOt%2Bedc0gB1VNZxe2vb6%2FECJqah%2FoukIu53GyrtAgiy%2F%2F%2F%2F%2F%2F%2F%2F%2F%2F8BEAEaDDEwMjU3MTQzODg2MyIMpekTO%2FwRnCERJLk6KsEC9y%2B9DXhMFpU1cQyunUpY80YaaBOOVsrwWqS5Jm3qUgLDZ%2B9zfKD4%2Bi3J7ui4Yj2ZbS%2F1cE3FXyzk2cIlsbOExvJKDzOazZKY5K0%2BnlFVUcnAK9dZ6jDExZEToGzNZeN7Or03ue2XqAeu02Uw3IGVAyj0sxnSjNuKpaOKfzDv9mgJGCtY8QqJKJVy9Nny6ZBXp0Mp0mySx3Xux8VnLJDnWFEjogua1QEgFFTorXbCNtWebYhZcZBN2TaHAsJMWYS7t8n1L8hEOiR%2BV%2FODtFFPjJMqyFm9q1zFamK6cc%2B7hjirWp0EU5yi65e4xN1Au9PXP6aHy3IKq9FOiKjb6jKYWiYGzOgaYhcoB2pvmLdUroK5I8rme2Fqgppszx88PM0ydpvExhLmJWyQ%2FOUK4TowZeebKZHDA4mnMbJtz%2BBpQdc1MPjj3pUGOrQCrfDSQC2wvcAHi3k7tBsdIj6H5%2F0dPXvvnZ3SOnTPT4jk%2FxWdKLRabsJX5E9WRQ0d9A1Squ2W%2BCxSMbnuo6CBM5x19zJcBTeefBnGN1%2FQ2suVB39%2BNbWNlvkGhTzrmOTLE7pC2sFrH1kHv9tYHQqEKfX3Cy5fYakMNAKsoE4PBmoBjonOzlfQa3MNmU96Il6n%2FqLBgSIeGCpuEJSKrZC%2BqpZ4IhgEx9J2EM6Yl0BG%2FXGEJyMZrzFTMMybktxEZHfzdoMgTLPWyW7lnaOx8RaeN6FZvOYjztM7iHDsiMC8TCMp4YThXNukgN%2FJ3ChlGylfVaHdHu4%2BDgWgvZdBk9%2Bo%2FdgoWTvZzkoy99RhhSSMUCIdtdTOhY2BqIocbEaYh4qur5l2Nr63Onry4%2F%2FuBt79gLAxeHY%3D&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Date=20220626T015123Z&X-Amz-SignedHeaders=host&X-Amz-Expires=43200&X-Amz-Credential=ASIARPYN36MH4XULSANX%2F20220626%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Signature=bb39e908509320a4fffd447b796ddfeb01837b89841c4deee876fd4d1e994774",
+        data: "https://wildfiredata2.s3.amazonaws.com/coral_bleaching.geoJson",
       });
 
       map.addLayer({
